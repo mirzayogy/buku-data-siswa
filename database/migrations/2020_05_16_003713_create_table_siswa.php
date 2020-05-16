d226be8a7e3e693b64c1f936f5a19d3d24d8769e
mirzayogy <mirza.yogy@gmail.com>
2020-05-16 08:49:04 +0800

    php artisan make:migration create_table_siswa
    php artisan migrate

---
+++ database/migrations/2020_05_16_003713_create_table_siswa.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableSiswa extends Migration
+{
+    public function up()
+    {
+        Schema::create('siswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('nisn',4)->unique();
+            $table->string('nama_siswa',30);
+            $table->date('tanggal_lahir');
+            $table->enum('jenis_kelamin',['L','P']);
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('siswa');
+    }
+}
